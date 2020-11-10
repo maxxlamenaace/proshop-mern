d9360aa8176740c595dcf07591d069b521281823
Maxime Beneteau <maxime.beneteau@reseau.eseo.fr>
2020-11-10 21:27:28 +0100

    Backend setup

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const products = require("./data/products");
+
+dotenv.config();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((product) => product._id === req.params.id);
+  res.json(product);
+});
+
+const PORT = process.env.PORT | 5000;
+const NODE_ENV = process.env.NODE_ENV;
+app.listen(
+  PORT,
+  console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`)
+);
